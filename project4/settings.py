e7c08b31de92be60c3051590fd8c1e1a97e2b55d
Folke <folke.bernroth@icloud.com>
2023-02-05 19:37:10 +0100

    change

--- project4/settings.py
+++ project4/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_DIRS = [
     os.path.join(BASE_DIR, "static")
