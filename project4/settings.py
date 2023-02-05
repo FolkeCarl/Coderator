6248d77a9485f17a2c63dfaa5a3e0808b7c987f5
Folke <folke.bernroth@icloud.com>
2023-02-05 21:45:51 +0100

    gay

--- project4/settings.py
+++ project4/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
