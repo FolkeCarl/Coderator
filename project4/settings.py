96d55b0bda84cb337f93a9e9376b2a9aebde9f25
Folke <folke.bernroth@icloud.com>
2023-02-06 20:58:44 +0100

    yessir

--- project4/settings.py
+++ project4/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'network/media')
 
 STATIC_URL = '/static/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
