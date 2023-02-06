fd7b0695a959c0e93f407774709a27ee3190b9a7
Folke <folke.bernroth@icloud.com>
2023-02-06 20:41:08 +0100

    new day

--- project4/settings.py
+++ project4/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'network/media')
+#MEDIA_URL = '/media/'
